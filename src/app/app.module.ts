adc858db009ff8ec9ce6f96259384dabe42e3c11
tayray10 <TRay@jhb.dvt.co.za>
2021-08-18 14:27:28 +0200

    DPP-9 Created basic login page
    - DPP-9 Added Angular Material to project
    - DPP-9 Added Deezer logo
    - DPP-9 Generated sign-in component
    - DPP-9 Added layout and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
