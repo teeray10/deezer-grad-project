38b16b31b6bb5bff43c791748ab6813f5a787c34
tayray10 <TRay@jhb.dvt.co.za>
2021-08-24 11:30:23 +0200

    DPP-3 Search bar now navigates to new route on submit
    	- Added router dependency
    	- Added function to navigate to new route with search term as param
    	- Added cursor pointer changes on icon hovers
    	- Added Router spy to test

--- src/app/features/navbar/navbar.component.spec.ts
+++ src/app/features/navbar/navbar.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+    let component: NavbarComponent;
+    let fixture: ComponentFixture<NavbarComponent>;
+    let routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+        declarations: [NavbarComponent],
+        providers: [
+            { provide: Router, useValue: routerSpy }
+        ]
     })
     .compileComponents();
   });
