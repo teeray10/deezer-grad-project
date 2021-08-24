89208511445efc601ea135535e10dc59b9f565f5
tayray10 <TRay@jhb.dvt.co.za>
2021-08-24 14:09:36 +0200

    DPP-26 Home component now passes data arrays to children

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Album } from 'src/app/models/album';
+import { Artist } from 'src/app/models/artist';
+import { FavoriteAlbumsService } from 'src/app/services/favorite-albums.service';
+import { FavoriteArtistsService } from 'src/app/services/favorite-artists.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+    favoriteAlbums!: Album[];
+    favoriteArtists!: Artist[];
+    unsubscribe$ = new Subject();
+
+    constructor(private favoriteAlbumsService: FavoriteAlbumsService, private favoriteArtistsService: FavoriteArtistsService) { }
+    
+    ngOnInit(): void {
+        this.getFavoriteAlbums();
+        this.getFavoriteArtists();
+    }
+    
+    private getFavoriteAlbums() {
+        this.favoriteAlbumsService.getFavoriteAlbums()
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe(albums => this.favoriteAlbums = albums);
+    }
+        
+    private getFavoriteArtists() {
+        this.favoriteArtistsService.getFavoriteArtists()
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe(artists => this.favoriteArtists = artists);
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 }
