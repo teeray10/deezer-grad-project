8deac34ba014df4271f88b35de1fdbf6dfe80435
tayray10 <TRay@jhb.dvt.co.za>
2021-08-25 10:14:38 +0200

    DPP-40 Added layout and styling of album component

--- src/app/features/album/album.component.ts
+++ src/app/features/album/album.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Album } from '../../models/album';
+import { Track } from '../../models/track';
+import { AlbumService } from '../../services/album/album.service';
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.scss']
 })
-export class AlbumComponent implements OnInit {
+export class AlbumComponent implements OnInit, OnDestroy {
+    albumId: string | null = '';
+    tracks: Track[] = [];
+    selectedAlbum!: Album;
+    unsubscribe$ = new Subject();
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private albumService: AlbumService) { }
+    
+    ngOnInit(): void {
+        this.albumId = this.getAlbumId();
+        this.getSelectedAlbum();
+    }
+    
+    getSelectedAlbum(): void {
+        this.albumService.getSelectedAlbum(this.albumId)
+        .pipe(takeUntil(this.unsubscribe$))
+            .subscribe(album => {
+                this.selectedAlbum = album;
 
-  ngOnInit(): void {
-  }
+                if(album.tracks)
+                    this.tracks = album.tracks.data;
+            });
+    }
+    
+    getAlbumId(): string | null {
+        return this.activatedRoute.snapshot.paramMap.get('id');
+    }
 
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 }
